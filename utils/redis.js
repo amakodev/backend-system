15f6b5ed12483fcf1f0c0f7ca71f6d32c252cfad
amakodev <amakodev@gmail.com>
2025-01-17 10:35:41 +0200

    redis fix attempt

--- utils/redis.js
+++ utils/redis.js
@@ -1,7 +1,10 @@
 const Redis = require('ioredis');
 
 // Connect to Redis using the URL from the environment variables
-const redis = new Redis(process.env.REDIS_URL);
+const redis = new Redis(process.env.REDIS_URL, {
+    maxRetriesPerRequest: null, // Required for BullMQ
+    enableReadyCheck: true,    // Ensures Redis is ready before using it
+  });
 
 // Log connection events
 redis.on('connect', () => console.log('Connected to Redis'));
